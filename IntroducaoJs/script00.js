3d0ff09bef55f796b3ef079d535be6e4ff687a31
caverzan-louco <caverzanpedroatilio8@gmail.com>
2025-02-06 11:31:33 -0300

    06.02.2025 introdução

---
+++ IntroducaoJs/script00.js
@@ -0,0 +1,35 @@
+const texto = "Olá, Mundo!";
+var boasVindas = "Bom dia, Magnata!";
+boasVindas = "Boas vindas alunos";
+/*texto = "hnjklç"*/
+
+console.log(texto);
+console.log(boasVindas);
+
+//Tipo de Dados//
+
+let string = "variavel de texto"; //Sempre entre aspas duplas ou simples
+let numero1 = 42; //numero inteiro
+let numero2 = 10.56; //numero decimal
+let numero3 = "2"; //numero em texto
+let boolean = true;
+let boolean2 = false;
+let boolean3 = "true"; //texto
+let undefined;
+let nula = null
+
+//let, var, const
+
+let resultado = true; //atribuição
+resultado = false; //reatribuição
+//let resultado = false  - proibido reescrever
+//let pode reatribuir mas não pode reescrever
+
+var nota = 10; //atribuição
+nota = 9; //reatribuição
+var nota = 8; //reescrita
+
+const fixo = 42;
+//fixo = 43; erro - não pode reatribuir 
+// const fixo = 41 - não pode reescrever
+
---
+++ IntroducaoJs/script01operacoes.js
@@ -0,0 +1,36 @@
+//Tipos de Operações em JavaScripts
+
+let a = 10 
+let b = 3
+
+
+console.log("soma")
+console.log(a+b);//13
+console.log("subtração")
+console.log(a-b);//7
+console.log("multiplicação")
+console.log(a*b);//30
+console.log("divisão")
+console.log(a/b);//3.333...
+console.log("resto da divisão")
+console.log(a%b);//1
+
+//Operadores Relacionais
+// (>, >=, <, <=, ==, ===)
+console.log(5>10);//false
+console.log(10 == "10");//true
+console.log(10 === "10");//false (porque não é do mesmo tipo)
+br;
+
+//Operadores Lógicos
+let nota1 = 5;
+let nota2 = 8;
+
+// our (||)
+console.log(nota1>=7 || nota2>=7); //true
+
+//and (&&)
+console.log(nota1>7 && nota2 >=7); //false
+
+//not (!)
+console.log(!true) //false
